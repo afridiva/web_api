531c88389e4b7341f2ccf726ff08f2baa1ad1976
Chineze <nwosuchineze@gmail.com>
2016-05-20 19:18:12 +0100

    add more user attributes

--- api.js
+++ api.js
@@ -1,27 +1,32 @@
 $(document).ajaxStart(function () {
+  $('.message').hide();
+}).ajaxError(function () {
+  $('.message').text('Resource not found');
+});
+
+$(window).load(function() {
   $('.facebook').hide();
   $('.github').hide();
-}).ajaxStop(function () {
-
-}).ajaxError(function () {
-
 });
 
 $(document).ready(function() {
-  // Hide result pages
-  $('.facebook').show();
-  $('.github').show();
+  $('.facebook').hide();
+  $('.github').hide();
 
   $(document).on('click', '#submitbtn', function(){
     var name = $('#param').val();
-    var type = $('#selectapi').val();
-    if(type != 0) {
-      getResponse(type + name + '?client_id=49f1860af7605ac87d8d&client_secret=9cd11a7b4ae32244dd500464adeb6c88e8a3a4b7');
+    var type_url = $('#selectapi').val();
+    if (name) {
+
+      if ($('#selectapi').val().includes('github')) {
+        name += '?client_id=49f1860af7605ac87d8d&client_secret=9cd11a7b4ae32244dd500464adeb6c88e8a3a4b7';
+      }
+
+      getResponse(type_url + name);
       return false;
     }
     else {
-      $('#message').append('No API selected. Displaying results for GitHub API...');
-      getResponse('https://api.github.com/users/' + name + '?client_id=49f1860af7605ac87d8d&client_secret=9cd11a7b4ae32244dd500464adeb6c88e8a3a4b7');
+      $('.message').text('Please provide a user name');
       return false;
     }
   });
@@ -31,18 +36,23 @@ function getResponse(url) {
   $.ajax({
     url: url
   }).then(function(data) {
-   $('.id').append(data.id);
-   $('.name').append(data.login);
-   $('.avatar').attr('src', data.avatar_url);
-   $('.facebook').show();
- });
-}
-
-function initMap() {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
-  });
+    if(data.id != null) {
+     $('.id').append(data.id);
+     $('.name').append(data.login);
+     $('.type').append(data.type);
+     $('.admin').append(data.site_admin);
+     $('.url').append(data.url);
+     $('.page').append(data.html_url);
+     $('.furl').append(data.followers_url);
+     $('.fgurl').append(data.following_url);
+     $('.cat').append(data.created_at);
+     $('.uat').append(data.updated_at);
+     $('.prepos').append(data.public_repos);
+     $('.avatar').attr('src', data.avatar_url);
+     $('.gitHub').show();
+   }
+   else {
+    $('.message').text('This user does not exist on ' + $('#selectapi').text());
+  }
+});
 }
